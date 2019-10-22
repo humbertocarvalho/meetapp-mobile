8d164f5be1f5084a8945028f1e2c1bce167ff887
Humberto de Carvalho <hvcarvalhobrt@gmail.com>
2019-10-22 07:23:18 -0300

    Add Dashboard Header.

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
+import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { withNavigationFocus } from 'react-navigation';
+import { format, addDays, subDays } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 
 import Background from '~/components/Background';
 import Meetup from '~/components/Meetup';
-import { Container, List } from './styles';
 import api from '~/services/api';
 import Header from '~/components/Header';
 
+import { Container, List, DashHeader, Title } from './styles';
+
 function Dashboard({ isFocused }) {
+  const [loading, setLoading] = useState(true);
   const [meetups, setMeetups] = useState([
     {
       past: false,
@@ -23,6 +28,12 @@ function Dashboard({ isFocused }) {
       id: 1,
     },
   ]);
+  const [date, setDate] = useState(new Date());
+
+  const dateFormatted = useMemo(
+    () => format(date, "d 'de' MMMM", { locale: pt }),
+    [date],
+  );
 
   async function loadMeetups() {
     const response = await api.get('meetups');
@@ -44,10 +55,27 @@ function Dashboard({ isFocused }) {
       ),
     );
   }
+
+  function handlePrevDay() {
+    setDate(subDays(date, 1));
+  }
+  function handleNextDay() {
+    setDate(addDays(date, 1));
+  }
+
   return (
     <Background>
       <Header />
       <Container>
+        <DashHeader>
+          <TouchableOpacity onPress={handlePrevDay}>
+            <Icon size={36} name="chevron-left" color="#fff" />
+          </TouchableOpacity>
+          <Title>{dateFormatted}</Title>
+          <TouchableOpacity onPress={handleNextDay}>
+            <Icon size={36} name="chevron-right" color="#fff" />
+          </TouchableOpacity>
+        </DashHeader>
         <List
           data={meetups}
           keyExtractor={item => String(item.id)}
--- src/pages/Dashboard/styles.js
+++ src/pages/Dashboard/styles.js
@@ -5,6 +5,19 @@ export const Container = styled.SafeAreaView`
   flex: 1;
 `;
 
+export const DashHeader = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  padding-top: 34px;
+`;
+
+export const Title = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #fff;
+`;
+
 export const List = styled.FlatList.attrs({
   showsVerticalScrollIndicator: false,
   contentContainerStyle: {
